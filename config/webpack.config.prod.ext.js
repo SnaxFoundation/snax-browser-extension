4009e0306eba1b9bd2f8041c4e62f50afa6d79fd
ant-mad <0.0.mad.ant@gmail.com>
2018-12-20 18:18:14 +0300

    Updated build

---
+++ config/webpack.config.prod.ext.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const webpack = require("webpack");
+const paths = require("./paths");
+
+module.exports = {
+  bail: true,
+  devtool: false,
+  entry: {
+    background: path.join(paths.appSrc, "background.js"),
+    injected: path.join(paths.appSrc, "injected.js"),
+    contentscript: path.join(paths.appSrc, "contentscript.js")
+  },
+  output: {
+    path: paths.appBuild,
+    filename: "[name].bundle.js"
+  },
+  resolve: {
+    modules: ["node_modules", paths.appDirectory],
+    extensions: [".js", ".json", ".jsx"]
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      {
+        oneOf: [
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve("url-loader"),
+            options: {
+              limit: 10000,
+              name: "static/media/[name].[hash:8].[ext]"
+            }
+          },
+          // Process JS with Babel.
+          {
+            test: /\.(js|jsx|mjs)$/,
+            include: paths.appSrc,
+            loader: require.resolve("babel-loader"),
+            options: {
+              compact: true
+            }
+          },
+          {
+            loader: require.resolve("file-loader"),
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            options: {
+              name: "static/media/[name].[hash:8].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)]
+};
