dcfc6b6f353bc7a8c36015aeb92c4c86dffaabf3
bee-warrior <casual.bee.warrior@gmail.com>
2019-02-16 20:37:55 +0300

    remove chedentials from twitter requests

--- src/services/accounts/AccountResolver.js
+++ src/services/accounts/AccountResolver.js
@@ -4,15 +4,25 @@ import { Singleton } from "src/context/steriotypes/Singleton";
 @Singleton
 export class AccountResolver {
   async getAccountNameById(id) {
-    let userPage = await fetch(`https://twitter.com/intent/user?user_id=${id}`);
+    let userPage = await fetch(
+      `https://twitter.com/intent/user?user_id=${id}`,
+      {
+        credentials: "omit"
+      }
+    );
+
     userPage = await userPage.text();
     const userName = this.extractNameFromPage(userPage);
     return userName;
   }
 
   async getIdByAccountName(accountName) {
-    let userPage = await fetch(`https://twitter.com/${accountName}`);
+    let userPage = await fetch(`https://twitter.com/${accountName}`, {
+      credentials: "omit"
+    });
+
     userPage = await userPage.text();
+    console.log(userPage);
     const id = this.extractIdFromPage(userPage);
     return id;
   }
