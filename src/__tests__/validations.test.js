127a90f37b288ea0cad03fd1c873613930c11c03
bee-warrior <casual.bee.warrior@gmail.com>
2019-02-21 19:22:19 +0300

    password validation tests

---
+++ src/__tests__/validations.test.js
@@ -0,0 +1,76 @@
+const { PasswordValidator } = require('../utils/validators/PasswordValidator');
+
+describe('validation functions test', () => {
+  it('empty password is not valid', () => {
+    const { isValid } = new PasswordValidator('');
+    expect(isValid).toBe(false);
+  });
+
+  it('password should contains min 8 characters', () => {
+    const sixCharacterValidationResult = new PasswordValidator('aabbcc');
+
+    expect(sixCharacterValidationResult.areMoreThan7CharactersUsed).toBe(false);
+    expect(sixCharacterValidationResult.isValid).toBe(false);
+
+    const eightCharacterValidationResult = new PasswordValidator('aabbccdd');
+
+    expect(eightCharacterValidationResult.areMoreThan7CharactersUsed).toBe(
+      true
+    );
+    expect(eightCharacterValidationResult.isValid).toBe(false);
+  });
+
+  it('passwordd can\t contain emoji', () => {
+    const {
+      areOnlyAlphanumericAndSpecialCharactersUsed,
+    } = new PasswordValidator('😎');
+
+    expect(areOnlyAlphanumericAndSpecialCharactersUsed).toBe(false);
+  });
+
+  it('alphanumeric and special characters are allowed', () => {
+    const {
+      areOnlyAlphanumericAndSpecialCharactersUsed,
+    } = new PasswordValidator('AAaa11-!#@$%^&*()_+|~=`{}[]:";<>?,.');
+
+    expect(areOnlyAlphanumericAndSpecialCharactersUsed).toBe(true);
+  });
+
+  it('password can\t contain spaces', () => {
+    const {
+      areOnlyAlphanumericAndSpecialCharactersUsed,
+    } = new PasswordValidator('hey snax');
+
+    expect(areOnlyAlphanumericAndSpecialCharactersUsed).toBe(false);
+  });
+
+  it('at least one uppercase and number or special character', () => {
+    const {
+      areAtLeastOneUppercaseAndNumberOrCharacter,
+      isValid,
+    } = new PasswordValidator('Qwerty123');
+
+    expect(areAtLeastOneUppercaseAndNumberOrCharacter).toBe(true);
+    expect(isValid).toBe(true);
+  });
+
+  it('at least one uppercase and number or special character', () => {
+    const {
+      areAtLeastOneUppercaseAndNumberOrCharacter,
+      isValid,
+    } = new PasswordValidator('Qwerty%!@');
+
+    expect(areAtLeastOneUppercaseAndNumberOrCharacter).toBe(true);
+    expect(isValid).toBe(true);
+  });
+
+  it('at least one uppercase and number or special character', () => {
+    const {
+      areAtLeastOneUppercaseAndNumberOrCharacter,
+      isValid,
+    } = new PasswordValidator('Qwerty');
+
+    expect(areAtLeastOneUppercaseAndNumberOrCharacter).toBe(false);
+    expect(isValid).toBe(false);
+  });
+});
