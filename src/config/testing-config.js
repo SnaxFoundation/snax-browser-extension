69b0cce28d04a464e7e8a92af47a1b81f96f6c6f
bee-warrior <casual.bee.warrior@gmail.com>
2019-03-23 20:31:51 +0300

    adds tests selectors for clipboard

--- src/config/testing-config.js
+++ src/config/testing-config.js
@@ -2,4 +2,6 @@ export const config = {
   chainUrl: 'http://localhost:3055',
   twitterUrl: 'http://localhost:3055/twitter',
   allowAllUnknownDomains: true,
+  // for tests
+  keepLastClipboardValue: true,
 };
